c7d8a9d22ead783bd0eec5a099e14d3ab20b1b00
BluishRed <bluishredstudios@gmail.com>
2022-07-19 10:10:00 +0100

    Removed Dimensions

---
+++ Source/Globals/Graphics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace BluishFramework
+{
+    public static class Graphics
+    {
+        public static Point GameResolution { get; set; }
+        public static Point ScreenResolution { get; set; }
+    }
+}
